93ca42cd8a441d806ed4a00acce052dc468e5f77
Edmond de Tournadre <edmond92@gmail.com>
2018-11-06 15:14:11 +0100

    work on new post form

---
+++ alumni-platform/resources/views/create_post.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Créez un nouvel article') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="title" class="col-sm-4 col-form-label text-md-right">Titre</label>
+
+                            <div class="col-md-6">
+                                <input id="title" type="text" name="title" placeholder="Mon Super Titre">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="type" class="col-sm-4 col-form-label text-md-right">Type</label>
+
+                            <div class="col-md-6">
+                                <select id="type" name="type">
+                                    <option>12</option>
+                                    <option>1000</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group justify-content-center">
+                            <label for="content" class="col-md-12 col-form-label text-md-center">Contenu de l'article</label>
+
+                            <div class="col-md-12 text-md-center">
+                                <textarea id="content" name="content" placeholder="Je vais vous parler aujourd'hui d'une fameuse technologie..." rows="30" cols="45"></textarea>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __("Envoyer l'article") }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
