6a6b8c35c293c3f39c3172d053c4419a69685d07
Edmond de Tournadre <edmond92@gmail.com>
2018-11-06 15:11:54 +0100

    add queries to populate db

--- db.sql
+++ db.sql
@@ -42,3 +42,63 @@ created_at VARCHAR(45),
 update_at VARCHAR(45),
 deleted_at VARCHAR(45)
 );
+
+
+
+## Populate
+
+INSERT INTO trainings (user_id, name, year, location)
+VALUES (
+	1,
+	'Développeur Web/Web Mobile',
+	'2018-2019',
+	'Saint-Gaudens'
+);
+
+
+INSERT INTO trainings (user_id, name, year, location)
+VALUES (
+	2,
+	'Référent Digital',
+	'2019-2020',
+	'Toulouse'
+);
+
+INSERT INTO trainings (user_id, name, year, location)
+VALUES (
+	3,
+	'Data IA',
+	'2018-2019',
+	'Montpellier'
+);
+
+
+INSERT INTO types (type)
+VALUES (
+	"Article"
+);
+
+INSERT INTO types (type)
+VALUES (
+	"Offre d'emploi"
+);
+
+INSERT INTO types (type)
+VALUES (
+	"Offre de stage"
+);
+
+INSERT INTO types (type)
+VALUES (
+	"Offre d'alternance"
+);
+
+INSERT INTO types (type)
+VALUES (
+	"Formation"
+);
+
+INSERT INTO types (type)
+VALUES (
+	"Ressource intéressante"
+);
