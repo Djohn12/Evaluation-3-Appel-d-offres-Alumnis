d0cf072d173eef350ebac73920e5f79b8113901f
Edmond de Tournadre <edmond92@gmail.com>
2018-11-07 11:25:52 +0100

    add 'author' column in table 'posts' for convenience and performance

--- alumni-platform/database/migrations/2018_11_06_114208_create_posts_table.php
+++ alumni-platform/database/migrations/2018_11_06_114208_create_posts_table.php
@@ -16,6 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
+            $table->string('author');
             $table->integer('author_id')->unsigned();
             $table->foreign('author_id')->references('id')->on('users');
             $table->string('content');
