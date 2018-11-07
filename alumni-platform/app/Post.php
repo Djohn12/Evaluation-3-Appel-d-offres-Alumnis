d527e04f94af1e8b07161e9c3557962b6fde94af
Edmond de Tournadre <edmond92@gmail.com>
2018-11-07 10:08:27 +0100

    define relations between models

--- alumni-platform/app/Post.php
+++ alumni-platform/app/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function type()
+    {
+    	return $this->belongsTo('App\Type');
+    }
+
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
 }
